4f7699571b23d32b64afc148463288b3be30c2bd
Lubomir92 <szlovakl333@gmail.com>
2026-09-06 12:33:48 +0200

    Fix: CORS - povolenie Vercel URL

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://task-manager-app-sepia-psi.vercel.app', 'https://task-manager-4u1n2ilkm-lubomir92-s-projects.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
